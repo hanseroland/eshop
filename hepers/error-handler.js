29c052b756037bc72d291b5ab6b1ed8aba3ccf03
hanseroland <rolandntougou@gmail.com>
2023-03-20 10:11:44 +0000

    orders

--- hepers/error-handler.js
+++ hepers/error-handler.js
@@ -1,7 +1,7 @@
 function errorHandler(err,req,res,next){
     //err jwt authentification
     if(err.name === 'UnauthorizedError'){
-       return res.status(401).json({message:"Utilisateur non authaurisé"});
+       return res.status(401).json({message:"Utilisateur non authaurisé - non admin"});
     }
 
     //err de validation
--- routes/users.js
+++ routes/users.js
@@ -39,6 +39,8 @@ router.post('/', async (req,res)=>{
    
 });
 
+
+
 router.post('/login', async (req,res)=>{
     let user = await User.findOne({
         email:req.body.email
@@ -51,7 +53,7 @@ router.post('/login', async (req,res)=>{
     if(user && bcrypt.compareSync(req.body.password,user.passwordHash)){
         const token = jwt.sign(
             {
-                userId:user.id,
+                userId:user.id, 
                 isAdmin:user.isAdmin
 
             },
@@ -66,5 +68,59 @@ router.post('/login', async (req,res)=>{
    
    
 });
+
+router.put('/:id', async (req,res)=>{
+    const userExist = await User.findById(req.params.id);
+    let newPassword;
+    if(req.body.password){
+        newPassword = bcrypt.hashSync(req.body.password,10)
+    }else{
+        newPassword = userExist.passwordHash;
+    }
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+        name:req.body.name,
+        email:req.body.email,
+        passwordHash:newPassword,
+        phone:req.body.phone,
+        isAdmin:req.body.isAdmin,
+        street:req.body.street,
+        apartment:req.body.apartment,
+        zip:req.body.zip,
+        country:req.body.country,
+    },{new:true}
+    );
+  
+    if(!user)
+        return res.status(404).send('L\'utilisateur ne peut pas être mis à jour');
+    res.send(user);
+   
+});
+
+router.delete('/:id', (req,res)=>{
+    User.findByIdAndRemove(req.params.id).then(user=>{
+         if(user){
+             return res.status(200).json({success:true,message:'Utilisateur supprimé'});
+         }else{
+             return res.status(404).json({success:false,message:'Utlilisateur non trouvé'});
+         }
+     }).catch(err=>{
+         return res.status(400).json({success:false,error:err});
+     });
+    
+ });
+
+router.get(`/get/count`,async (req,res)=>{
+    const userCount = await User.countDocuments();
+    if(!userCount){
+        res.status(500).json({success:false});
+    }
+    res.send({
+        count:userCount
+    });
+});
+
+
 
 module.exports = router;
